5fd2922845ce6f48483351308f4935495cb584cf
pik94 <pinchuk.ilya@inbox.ru>
2017-11-09 17:18:13 +0300

    Added the second week's tasks

---
+++ course_1/week_2/linear_algebra/approximation_of_function/main.py
@@ -0,0 +1,78 @@
+# coding: utf-8
+# Python's version: 3.5
+# Задача 2: аппроксимация функций
+
+
+from math import sin, exp
+import numpy as np
+from scipy.linalg import solve
+from matplotlib import pyplot as plt
+
+
+def f(x):
+    return sin(x / 5) * exp(x / 10) + 5 * exp(-x / 2)
+
+
+def plot(x, y, x_name, y_name):
+    """
+    Метод строит график данных y=f(x).
+    :param x: list, ndarray или другой подобный тип
+    :param y: list, ndarray
+    :param x_name: string
+    :param y_name: string
+    :return: None
+    """
+    plt.plot(x, y)
+    plt.xlabel(x_name)
+    plt.ylabel(y_name)
+    plt.show()
+    return
+
+
+def start_linear_approximation(show_plot=False):
+    x_array = [1, 15]
+    A = [[1, x_array[0]],
+         [1, x_array[1]]]
+    b = [f(x) for x in x_array]
+    b = np.array(b)
+    A = np.array(A)
+    coefs = solve(A, b)
+    if show_plot:
+        plot(x_array, [coefs[0] + coefs[1] * x for x in x_array], "x", "y")
+    return
+
+
+def start_quadratic_approximation(show_plot=False):
+    x_array = [1, 8, 15]
+    A = [[1, x_array[0], x_array[0] * x_array[0]],
+         [1, x_array[1], x_array[1] * x_array[1]],
+         [1, x_array[2], x_array[2] * x_array[2]]]
+    b = [f(x) for x in x_array]
+    b = np.array(b)
+    A = np.array(A)
+    coefs = solve(A, b)
+    if show_plot:
+        plot(x_array, [coefs[0] + coefs[1] * x + coefs[2] * (x ** 2) for x in x_array], "x", "y")
+    return
+
+
+def start_third_deg_approximation(show_plot=False):
+    x_array = [1, 4, 10, 15]
+    A = [[1, x_array[0], x_array[0] * x_array[0], x_array[0] * x_array[0] * x_array[0]],
+         [1, x_array[1], x_array[1] * x_array[1], x_array[1] * x_array[1] * x_array[1]],
+         [1, x_array[2], x_array[2] * x_array[2], x_array[2] * x_array[2] * x_array[2]],
+         [1, x_array[3], x_array[3] * x_array[3], x_array[3] * x_array[3] * x_array[3]]]
+    b = [f(x) for x in x_array]
+    b = np.array(b)
+    A = np.array(A)
+    coefs = solve(A, b)
+    if show_plot:
+        plot(x_array, [coefs[0] + coefs[1] * x + coefs[2] * (x ** 2) + coefs[3] * (x ** 3) for x in x_array], "x", "y")
+    return coefs
+
+
+if __name__ == "__main__":
+    # start_linear_approximation(True)
+    # start_quadratic_approximation(True)
+    coefs = start_third_deg_approximation(True)
+    print(list(map(lambda x: x.round(2), coefs)))
---
+++ course_1/week_2/linear_algebra/compare_sentences/main.py
@@ -0,0 +1,65 @@
+# coding: utf-8
+# Python's version: 3.5
+# Задача 1: сравнение предложений
+
+import re
+import numpy as np
+from scipy.spatial.distance import cosine
+
+
+def prepare_text():
+    with open("sentences.txt", "r") as file:
+        text = {}
+        sentence_count = 0
+        for line in file:
+            line = line.lower()
+            line = re.split(r"[^a-z]*", line)
+            while "" in line:
+                ind = line.index("")
+                line.remove(line[ind])
+            text[sentence_count] = line
+            sentence_count += 1
+    return text
+
+
+def tokenize_text(text):
+    tokens = {}
+    d = 0
+    for key in text.keys():
+        for word in text[key]:
+            if word not in tokens:
+                tokens[word] = d
+                d += 1
+    return tokens
+
+
+def create_matrix(text, tokens):
+    matrix = np.zeros((len(text), len(tokens)))
+    for key in text.keys():
+        sentence = text[key]
+        for tok_word in tokens.keys():
+            count = 0
+            for word in sentence:
+                if word == tok_word:
+                    count += 1
+            matrix[key, tokens[tok_word]] = count
+    return matrix
+
+
+def compute_distance(sentence, matrix):
+    distances = {}
+    for i in range(1, matrix.shape[0]):
+        distances[i] = cosine(sentence, matrix[i])
+    return distances
+
+
+
+if __name__ == "__main__":
+    text = prepare_text()
+    tokens = tokenize_text(text)
+    matrix = create_matrix(text, tokens)
+    distances = compute_distance(matrix[0], matrix)
+    distances = sorted(distances.items(), key=lambda item: item[1])
+    print(distances[0], distances[1])
+
+
